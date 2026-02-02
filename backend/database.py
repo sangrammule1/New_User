176e3576d85f91032552e0c5d67ac03234f32ae1
padmini@xinifynetworks.com <padmini@xinifynetworks.com>
2026-02-02 11:21:44 -0500

    database change

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String, Boolean
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:password@localhost/user_db"
+DATABASE_URL = "mysql+pymysql://root:jvm007@localhost/sys"
 # change user, password if needed
 
 engine = create_engine(DATABASE_URL)
